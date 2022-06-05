40cac415797ea0d80d97720ec6f0f8efc6061f3c
andys <andysmith836@gmail.com>
2022-06-05 22:54:37 +0100

    Get some instructions decoded

--- chip8.go
+++ chip8.go
@@ -5,6 +5,43 @@ type instruction struct {
 	second byte
 }
 
-func decodeInstruction(bytes [2]byte) instruction {
-	return instruction{bytes[0], bytes[1]}
+type memory struct {
+	bytes [4096]byte
+	pc    int
+}
+
+const (
+	Jump = iota
+	SetRegister
+	AddValueToRegister
+	SetIndexRegister
+)
+
+func (m *memory) load(bytes []byte) {
+	copy(m.bytes[:], bytes)
+}
+
+func (m memory) fetch() instruction {
+	return instruction{m.bytes[0], m.bytes[1]}
+}
+
+func (m *memory) decode() int {
+	firstByte := m.bytes[0]
+	mask := byte(0b11110000)
+
+	firstNibble := firstByte & mask
+
+	if firstNibble == 0x10 {
+		return Jump
+	}
+
+	if firstNibble == 0x60 {
+		return SetRegister
+	}
+
+	if firstNibble == 0x70 {
+		return AddValueToRegister
+	}
+
+	return SetIndexRegister
 }
--- chip8_test.go
+++ chip8_test.go
@@ -6,10 +6,62 @@ import (
 )
 
 func TestDecodeByte(t *testing.T) {
-	instruction := [2]byte {0x12, 0x20}
+	m := memory{}
+	instruction := []byte{0x12, 0x20}
 
-	decoded := decodeInstruction(instruction)
+	m.load(instruction)
+
+	decoded := m.fetch()
 	assert.Equal(t, byte(0x12), decoded.first, "First nibble")
 	assert.Equal(t, byte(0x20), decoded.second, "Second nibble")
 }
 
+func TestDecodeJump(t *testing.T) {
+	m := memory{}
+	instruction := []byte{0x1A, 0x2B}
+
+	m.load(instruction)
+
+	decoded := m.decode()
+	assert.Equal(t, Jump, decoded, "Jump")
+}
+
+func TestDecodeSetRegister(t *testing.T) {
+	m := memory{}
+	instruction := []byte{0x60, 0x20}
+
+	m.load(instruction)
+
+	decoded := m.decode()
+	assert.Equal(t, SetRegister, decoded, "Set Register")
+}
+
+func TestDecodeAddValueToRegister(t *testing.T) {
+	m := memory{}
+	instruction := []byte{0x70, 0x20}
+
+	m.load(instruction)
+
+	decoded := m.decode()
+	assert.Equal(t, AddValueToRegister, decoded, "Add value to register")
+}
+
+func TestDecodeAddValueToRegister2(t *testing.T) {
+	m := memory{}
+	instruction := []byte{0x77, 0x20}
+
+	m.load(instruction)
+
+	decoded := m.decode()
+	assert.Equal(t, AddValueToRegister, decoded, "Add value to register")
+}
+
+func TestDecodeSetIndexRegisterI(t *testing.T) {
+	m := memory{}
+	instruction := []byte{0xA5, 0x55}
+
+	m.load(instruction)
+
+	decoded := m.decode()
+	assert.Equal(t, SetIndexRegister, decoded, "Set index register")
+}
