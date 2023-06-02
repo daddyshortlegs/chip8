62c5802b195a212a6dcb92f154756da140339fb0
andys <andysmith836@gmail.com>
2023-06-02 22:52:39 +0100

    Added more complicated program to assemble

--- chip8/assembler_test.go
+++ chip8/assembler_test.go
@@ -46,39 +46,80 @@ func (suite *AssemblerTestSuite) TestGetKey() {
 	suite.Equal([]byte{0xF5, 0x0A}, theAssembler.assemble())
 }
 
+func (suite *AssemblerTestSuite) TestProgram() {
+	theAssembler := NewAssembler()
+	theAssembler.setRegister(0, 0x19)
+	theAssembler.setRegister(1, 0x00)
+
+	theAssembler.setRegister(2, 0x20)
+	theAssembler.setRegister(3, 0x00)
+
+	theAssembler.setRegister(4, 0x1E)
+	theAssembler.setRegister(5, 0x00)
+
+	theAssembler.setRegister(6, 0x23)
+	theAssembler.setRegister(7, 0x00)
+
+	theAssembler.setRegister(8, 0x28)
+	theAssembler.setRegister(9, 0x00)
+
+	theAssembler.setIndexRegister(0x50)
+	theAssembler.display(0, 1, 5)
+
+	theAssembler.setIndexRegister(0x55)
+	theAssembler.display(2, 3, 5)
+
+	theAssembler.setIndexRegister(0x5A)
+	theAssembler.display(4, 5, 5)
+
+	theAssembler.setIndexRegister(0x5F)
+	theAssembler.display(6, 7, 5)
+
+	theAssembler.setIndexRegister(0x64)
+	theAssembler.display(8, 9, 5)
+
+	theAssembler.getKey(3)
+
+	andysProgram := []byte{
+		0x60, 0x19,
+		0x61, 0x00,
+
+		0x62, 0x20,
+		0x63, 0x00,
+
+		0x64, 0x1E,
+		0x65, 0x00,
+
+		0x66, 0x23,
+		0x67, 0x00,
+
+		0x68, 0x28,
+		0x69, 0x00,
+
+		0xA0, 0x50,
+		0xD0, 0x15,
+
+		0xA0, 0x55,
+		0xD2, 0x35,
+
+		0xA0, 0x5A,
+		0xD4, 0x55,
+
+		0xA0, 0x5F,
+		0xD6, 0x75,
+
+		0xA0, 0x64,
+		0xD8, 0x95,
+
+		0xF3, 0x0A,
+	}
+	suite.Equal(andysProgram, theAssembler.assemble())
+}
+
 func TestAssemblerTestSuite(t *testing.T) {
 	suite.Run(t, new(AssemblerTestSuite))
 }
 
 /*
-andysProgram := []byte{
-0x60, 0x19, // Set register 0 to 0x00
-0x61, 0x00, // Set register 1 to 0x00
 
-0x62, 0x20, // Set register 2 to 0x05
-0x63, 0x00, // Set register 3 to 0x00
-
-0x64, 0x1E, // Set register 4 to 10
-0x65, 0x00, // Set register 5 to 0x00
-
-0x66, 0x23, // Set register 6 to 15
-0x67, 0x00, // Set register 7 to 0x00
-
-0x68, 0x28, // Set register 8 to 20
-0x69, 0x00, // Set register 9 to 0x00
-
-0xA0, 0x50, // Set Index Register to 0x50
-0xD0, 0x15, // Draw, Xreg = 5, Y reg = 10, 5 bytes high
-
-0xA0, 0x55, // Set Index Register to 0x55
-0xD2, 0x35, // Draw, Xreg = 5, Y reg = 10, 5 bytes high
-
-0xA0, 0x5A, // Set Index Register to 0x55
-0xD4, 0x55, // Draw, Xreg = 5, Y reg = 10, 5 bytes high
-0xA0, 0x5F, // Set Index Register to 0x5F
-0xD6, 0x75, // Draw, Xreg = 5, Y reg = 10, 5 bytes high
-0xA0, 0x64, // Set Index Register to 0x5F
-0xD8, 0x95, // Draw, Xreg = 5, Y reg = 10, 5 bytes high
-0xF3, 0x0A, // Wait for key
-}
-*/
+ */
