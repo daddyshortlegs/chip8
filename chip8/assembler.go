f2bbb516890be63e088959f951ed9f450b4f9c07
andys <andysmith836@gmail.com>
2023-06-04 22:10:51 +0100

    Slight clean up

--- chip8/assembler.go
+++ chip8/assembler.go
@@ -10,31 +10,27 @@ func NewAssembler() *Assembler {
 	return a
 }
 
-func (a *Assembler) SetRegister(index byte, value byte) {
-	register := 0x60 + index
-	opcodes := []byte{register, value}
+func (a *Assembler) buildArray(key byte, value byte) {
+	opcodes := []byte{key, value}
 	a.code = append(a.code, opcodes...)
+}
+
+func (a *Assembler) SetRegister(index byte, value byte) {
+	a.buildArray(0x60+index, value)
 }
 
 func (a *Assembler) SetIndexRegister(value uint16) {
-	first := extractFirstByte(value)
-	second := extractSecondByte(value)
-	instruction := 0xA0 | first
-	opcodes := []byte{instruction, second}
-	a.code = append(a.code, opcodes...)
+	instruction := 0xA0 | extractFirstByte(value)
+	a.buildArray(instruction, extractSecondByte(value))
 }
 
 func (a *Assembler) Display(xRegister byte, yRegister byte, n byte) {
-	instruction := 0xD0 | xRegister
 	second := (yRegister << 4) | n
-	opcodes := []byte{instruction, second}
-	a.code = append(a.code, opcodes...)
+	a.buildArray(0xD0|xRegister, second)
 }
 
 func (a *Assembler) GetKey(xRegister byte) {
-	instruction := 0xF0 | xRegister
-	opcodes := []byte{instruction, 0x0A}
-	a.code = append(a.code, opcodes...)
+	a.buildArray(0xF0|xRegister, 0x0A)
 }
 
 func (a *Assembler) Assemble() []byte {
